ccb0f2d6bda47895ea2fc4aa8d130cabe7751335
Silvio Molina <kindermolina98@gmail.com>
2020-09-12 19:58:44 -0300

    Exercise 21 wise done

---
+++ Makefile
@@ -0,0 +1,6 @@
+CC= gcc
+
+polaca: main.c
+	$(CC) main.c -o main
+auster: index.c
+	$(CC) index.c -o index
--- main.c
+++ main.c
@@ -4,35 +4,35 @@
 int main (){
     int i,contM,contB,contC,contT,cantvinateros,cont;
     char letra;
-    float cantuva,contador,promedio;
+    float cantuva,totaluva,promedio;
     contM=contB=contC=contT=cont=0;
     contador=promedio=0;
     printf("Cantidad de viñateros \n");
     scanf("%d", &cantvinateros);
      for (i=1;i<=cantvinateros;i++) {
-    printf("Cuantas uvas cosechaste \n");
-       scanf("%f", &cantuva);
-      printf("Que tipo de uva \n");
+        printf("Cuantas uvas cosechaste \n");
+        scanf("%f", &cantuva);
+        printf("Que tipo de uva \n");
         fflush(stdin);
-       letra=getchar();
+        letra=getchar();
         if ( letra == 'm' ) {
-          contM+= cantuva ;
-          cont += cantuva;
-     }else if (letra == 'b') {
-        contB+= cantuva ;
-        cont += cantuva;
-    } else if (letra =='t') {
-        contT+= cantuva;
-      } else {
-        contC+= cantuva;
-        }
-        contador += cantuva;
-      printf("La cantidad de uva %f\n",contador);
-}
-        promedio=(float)cont * 100 / contador;
-       printf("Mostrar el total de uvas cosechadas:  %f\n", contador);
-       printf("El porcentaje de uva moscatel y blanca: %f\n",promedio);
-       printf("La cantidad de kd cosechados de moscatel son: %d\n la cantidad de kg cosechados de Blanca son: %d\n la cantidad de kg cosechados de Comunes son: %d\n la cantidad de kg cosechados de Tintas son: %d\n",contM,contB,contC,contT);
+            contM+= cantuva ;
+            cont += cantuva;
+        }else if (letra == 'b') {
+            contB+= cantuva ;
+            cont += cantuva;
+            } else if (letra =='t') {
+                contT+= cantuva;
+                } else {
+                    contC+= cantuva;
+                }
+        totaluvas += cantuva;
+    }
+	promedio=(float)cont * 100 / totaluvas;
 
-         return 0;
+    printf("Total de uvas cosechadas:  %f\n", totaluvas);
+    printf("Porcentaje de uva moscatel y blanca: %f\n",promedio);
+    printf("La cantidad de kd cosechados de moscatel son: %d\n la cantidad de kg cosechados de Blanca son: %d\n la cantidad de kg cosechados de Comunes son: %d\n la cantidad de kg cosechados de Tintas son: %d\n",contM,contB,contC,contT);
+
+    return 0;
    }
