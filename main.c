ff29d80a0c8a43a6ac2ee1c612125267e2e66bd5
_moliNasil <41929823+NahuelMolina@users.noreply.github.com>
2020-09-12 15:23:26 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,46 +1,38 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    int cant = 0;
-    int EachWorker[cant];
-    int i; 
-    char tipo[20];
-    char tipoAnte[20];
-    int firsType = 0, secondType =0, thirdType=0, fourType=0;
-
-    printf("Ingresa la cantidad de viñateros");
-    scanf("%i", &cant);
-
-    for (i=0;i<cant;i++){
+int main (){
+    int i,contM,contB,contC,contT,cantvinateros,cont;
+    char letra;
+    float cantuva,contador,promedio;
+    contM=contB=contC=contT=cont=0;
+    contador=promedio=0;
+    printf("Cantidad de viñateros \n");
+    scanf("%d", &cantvinateros);
+     for (i=1;i<=cantvinateros;i++) {
+    printf("Cuantas uvas cosechaste \n");
+       scanf("%f", &cantuva);
+      printf("Que tipo de uva \n");
         fflush(stdin);
-        printf("Ingresa la cantidad de uva para el %i viñatero:\n", i+1);
-        scanf("%i", &EachWorker[i]);
-        fflush(stdin);
-        printf("Ingresa el tipo de uva que produce:\n");
-        scanf("%s",&tipo);
-
-        if(tipo == "moscatel"){
-            firsType = firsType + EachWorker[i];
-        }else if (tipo == "comunes"){
-            secondType = secondType + EachWorker[i];
-        }else if ("blancas"){
-            thirdType = thirdType + EachWorker[i];
-        }else {
-            fourType = fourType + EachWorker[i];
-    
+       letra=getchar();
+        if ( letra == 'm' ) {
+          contM+= cantuva ;
+          cont += cantuva;
+     }else if (letra == 'b') {
+        contB+= cantuva ;
+        cont += cantuva;
+    } else if (letra =='t') {
+        contT+= cantuva;
+      } else {
+        contC+= cantuva;
         }
-    };
-
-    printf("Cantidad de cada trabajador");
-    for(i=0;i<cant;i++){
-        printf("Para el %i: %i",i+1 ,EachWorker[i]);    
-    };
-
-    printf("La cantidad producida de moscatel total es: %i \n", firsType);
-    printf("de comunes es: %i \n", secondType);
-    printf("blancas es: %i \n", thirdType);
-    printf("de la otra nose cual es de: %i \n", fourType);
-    
-    return 0 ;
+        contador += cantuva;
+      printf("La cantidad de uva %f\n",contador);
 }
+        promedio=(float)cont * 100 / contador;
+       printf("Mostrar el total de uvas cosechadas:  %f\n", contador);
+       printf("El porcentaje de uva moscatel y blanca: %f\n",promedio);
+       printf("La cantidad de kd cosechados de moscatel son: %d\n la cantidad de kg cosechados de Blanca son: %d\n la cantidad de kg cosechados de Comunes son: %d\n la cantidad de kg cosechados de Tintas son: %d\n",contM,contB,contC,contT);
+
+         return 0;
+   }
